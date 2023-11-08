33a85541242c42e6803086a79828eebda44805f1
chichoc <kny6152@naver.com>
2023-11-08 20:50:04 +0900

    feat: 로또 구입 금액 입력받는 함수 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 class App {
-  async play() {}
+  async play() {
+    const amount = await this.getPurchaseAmount();
+
+  }
+
+  async getPurchaseAmount() {
+    return await Console.readLineAsync(`구입금액을 입력해 주세요.\n`);
+  }
+
 }
 
 export default App;
