e04c2c0fbd7163f9bfbe3c39a9d2a03edca41455
chichoc <kny6152@naver.com>
2023-11-08 21:03:43 +0900

    feat: 수량만큼 로또 발행 및 출력

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Random, Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
     const amount = await this.getPurchaseAmount();
 
     const count = this.calculateQuantity(amount);
     Console.print(`\n${count}개를 구매했습니다.`);
+    this.createLotto(count);
   }
 
   async getPurchaseAmount() {
@@ -13,6 +15,14 @@ class App {
   calculateQuantity(amount) {
     if (amount % 1000) throw new Error('[ERROR] 구입 금액이 잘못된 형식입니다.');
     return amount / 1000;
+  }
+
+  createLotto(num) {
+    for (let i = 0; i < num; i++) {
+      const newLotto = Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
+      Console.print(newLotto);
+      this.lottos.push(newLotto);
+    }
   }
 }
 
