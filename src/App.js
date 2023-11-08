bc0bd638698a82223334ae22875543dcaf10eace
chichoc <kny6152@naver.com>
2023-11-08 20:56:21 +0900

    feat: 로또 수량 계산 및 출력 - 로또 수량 계산하는 함수 생성 - 1,000원으로 나누어 떨어지지 않는 경우 에러 처리

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ class App {
   async play() {
     const amount = await this.getPurchaseAmount();
 
+    const count = this.calculateQuantity(amount);
+    Console.print(`\n${count}개를 구매했습니다.`);
   }
 
   async getPurchaseAmount() {
     return await Console.readLineAsync(`구입금액을 입력해 주세요.\n`);
   }
 
+  calculateQuantity(amount) {
+    if (amount % 1000) throw new Error('[ERROR] 구입 금액이 잘못된 형식입니다.');
+    return amount / 1000;
+  }
 }
 
 export default App;
